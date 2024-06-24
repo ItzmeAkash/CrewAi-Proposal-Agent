b78dcc89daa4444b43c35665843cc4a6eacb0003
akashps <itzmeakashps@gmail.com>
2024-06-24 17:12:09 +0530

    added

--- proposalagent/crewapp/tools/GoogleDrive/google_drive_tools.py
+++ proposalagent/crewapp/tools/GoogleDrive/google_drive_tools.py
@@ -6,6 +6,11 @@ from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 import re
 from crewai_tools import BaseTool
+from dotenv import load_dotenv
+
+
+#load enviroment variables from .env file
+load_dotenv()
 
 # Define the scopes required for accessing Google Drive API
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
@@ -57,8 +62,19 @@ class GoogleDriveDownloaderTool(BaseTool):
             if creds and creds.expired and creds.refresh_token:
                 creds.refresh(Request())
             else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    'credentials.json', SCOPES)
+                client_secrets = {
+                    "installed": {
+                        "client_id": os.getenv('GOOGLE_CLIENT_ID'),
+                        "project_id": os.getenv('GOOGLE_PROJECT_ID'),
+                        "auth_uri": os.getenv('GOOGLE_AUTH_URI'),
+                        "token_uri": os.getenv('GOOGLE_TOKEN_URI'),
+                        "auth_provider_x509_cert_url": os.getenv('GOOGLE_AUTH_PROVIDER_X509_CERT_URL'),
+                        "client_secret": os.getenv('GOOGLE_CLIENT_SECRET'),
+                        "redirect_uris": [os.getenv('GOOGLE_REDIRECT_URI')],
+                        "refresh_token": os.getenv('GOOGLE_REFRESH_TOKEN')
+                    }
+                }
+                flow = InstalledAppFlow.from_client_config(client_secrets,SCOPES)
                 creds = flow.run_local_server(port=0)
             with open('token.json', 'w') as token:
                 token.write(creds.to_json())
